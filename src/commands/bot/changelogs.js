f6cbb9eb85d5e16cff58bff39b6d6fede7c4bce8
Zʏʀᴇx Sᴇʀ <93309693+zZyreXx@users.noreply.github.com>
2024-01-22 13:04:03 +0530

    Update changelogs.js
    TEDX

--- src/commands/bot/changelogs.js
+++ src/commands/bot/changelogs.js
@@ -2,17 +2,15 @@ const Discord = require('discord.js');
 
 module.exports = async (client, interaction, args) => {
     client.embed({
-        title: "📃・Changelogs",
+        title: "<:logzz:1071794113982255134>・Changelogs",
         desc: `_____`,
         thumbnail: client.user.avatarURL({ size: 1024 }),
         fields: [{
-            name: "📃┆Changelogs",
-                value: '15/3/2023 Updated dependencies',
+            name: "<:logzz:1071794113982255134>┆Changelogs",
+                value: '10/12/2022 - Updated the bot to the latest version of discord.js (v14)',
                 inline: false,
             },
         ],
         type: 'editreply'
     }, interaction)
 }
-
- 
