a9588f05cc4e666988afb492516648084394f75f
Zʏʀᴇx Sᴇʀ <93309693+zZyreXx@users.noreply.github.com>
2024-01-23 14:43:25 +0530

    Update changelogs.js
    change radio

--- src/handlers/helppanel/changelogs.js
+++ src/handlers/helppanel/changelogs.js
@@ -54,6 +54,7 @@ module.exports = async (client) => {
                         {
                             name: "<:jsszz:1071798658409967747>┆Changelogs",
                             value: '10/12/2022 - Updated the bot to the latest version of discord.js (v14)',
+                            description: 'Added Mallu Radio Station Club FM 94.3', 
                             inline: false,
                         }
                     ],
