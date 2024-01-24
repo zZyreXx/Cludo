1ca8704caba585d9860f746bf9b4010bd66cd1a5
Zʏʀᴇx Sᴇʀ <93309693+zZyreXx@users.noreply.github.com>
2024-01-24 17:52:49 +0530

    Update changelogs.js
    New Updates

--- src/commands/bot/changelogs.js
+++ src/commands/bot/changelogs.js
@@ -7,7 +7,7 @@ module.exports = async (client, interaction, args) => {
         thumbnail: client.user.avatarURL({ size: 1024 }),
         fields: [{
             name: "<:logzz:1071794113982255134>┆Changelogs",
-                value: '10/12/2022 - Added Mallu Radio Station MAcfast 90.4a',
+                value: '24/01/2024 - Added [Mallu Radio Station Club FM 99.6](https://clubfm.ae/)\n -Fixed Minor Bugs\n -Changed Host\n -Updated bot Latest Disocrd.js/Music Version 0.16.1',
                 inline: false,
             },
         ],
