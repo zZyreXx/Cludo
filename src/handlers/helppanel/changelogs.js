2fc5143073b19000ee7a8952ec5e75cd6b673b29
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-02-09 23:26:29 +0530

    Update changelogs.js
    Fix

--- src/handlers/helppanel/changelogs.js
+++ src/handlers/helppanel/changelogs.js
@@ -12,48 +12,48 @@ module.exports = async (client) => {
                     .addComponents(
                         new Discord.StringSelectMenuBuilder()
                             .setCustomId('Bot-helppanel')
-                            .setPlaceholder('❌┆Nothing selected')
+                            .setPlaceholder('Navigation System')
                             .addOptions([
                                 {
                                     label: `Commands`,
                                     description: `Show the commands of Bot!`,
-                                    emoji: "<:compzz:1071795353445220432>",
+                                    emoji: "<:cmd:1205416566141886506>",
                                     value: "commands-Bothelp",
                                 },
                                 {
                                     label: `Invite`,
                                     description: `Invite Bot to your server`,
-                                    emoji: "<:linkee:1071784295594737768>",
+                                    emoji: "<:linkee:1205416083469762610>",
                                     value: "invite-Bothelp",
                                 },
                                 {
                                     label: `Support server`,
                                     description: `Join the suppport server`,
-                                    emoji: "<:helpzz:1072010469142974504>",
+                                    emoji: "<:__:1205407307811262466>",
                                     value: "support-Bothelp",
                                 },
                                 {
                                     label: `Changelogs`,
                                     description: `Show the bot changelogs`,
-                                    emoji: "<:i_:1071780309865091173>",
+                                    emoji: "<:i_:1205412741087494174>",
                                     value: "changelogs-Bothelp",
                                 },
                             ]),
                     );
 
                 client.embed({
-                    title: "<:i_:1071780309865091173>・Changelogs",
+                    title: "<:i_:1071780309865091173><:arrowright:1205414889451954196>Changelogs",
                     desc: `_____`,
                     thumbnail: client.user.avatarURL({ size: 1024 }),
                     fields: [
             	        {
-                            name: "<:ann:1071789734734610483>┆Alert!",
+                            name: "<:megaz:1205540030055981116><:arrowright:1205414889451954196>Alert!",
                             value: 'This is the changelogs of the bot, here you can see the changes that have been made to the bot.',
                             inline: false,
                         },
                         {
-                            name: "<:jsszz:1071798658409967747>┆Changelogs",
-                            value: '24/01/2024 - Added [Mallu Radio Station Club FM 99.6](https://clubfm.ae/)\n -Fixed Minor Bugs\n -Changed Host\n -Updated bot Latest Disocrd.js/Music Version 0.16.1',
+                            name: "<:i_:1205412741087494174><:arrowright:1205414889451954196>Changelogs",
+                            value: '24/01/2024 - Added [Mallu Radio Station Club FM 99.6](https://clubfm.ae/)\n -Updated To new UI',
                             inline: false,
                         }
                     ],
