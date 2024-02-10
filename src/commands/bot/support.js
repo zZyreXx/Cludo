96f798546148bc9d5fa9c2246131bfba1292d3a4
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-02-10 19:20:39 +0530

    Update support.js
    Fix

--- src/commands/bot/support.js
+++ src/commands/bot/support.js
@@ -10,9 +10,9 @@ module.exports = async (client, interaction, args) => {
         );
 
     client.embed({
-        title: `<:helpzz:1072010469142974504>・Support`,
+        title: `<:__:1205407307811262466>・Support`,
         desc: `Make your server even better with Bot!`,
-        image: "https://cdn.discordapp.com/attachments/1064478382504550400/1072035974781091960/CLUDO-removebg-preview.png",
+        image: "https://cdn.discordapp.com/attachments/1064478382504550400/1205446107279265812/CLUDO_1_2.png?ex=65d8662c&is=65c5f12c&hm=1dc3f94e83b96c83f3399ac2dafa88fa8c7a9479276a80371507b09e93e79950&",
         url: client.config.discord.serverInvite,
         components: [row],
         type: 'editreply'
