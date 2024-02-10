e936f8c596287f977b53d31e26306180fd39c602
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-02-10 09:19:19 +0530

    Update vote.js
    FIx

--- src/commands/bot/vote.js
+++ src/commands/bot/vote.js
@@ -1,44 +1,41 @@
 const Discord = require('discord.js');
-const Topgg = require('@top-gg/sdk');
+const Topgg = require(`@top-gg/sdk`);
 const moment = require("moment");
 require("moment-duration-format");
 
 module.exports = async (client, interaction, args) => {
     let dbl = new Topgg.Api(process.env.TOPGG_TOKEN)
 
-    let row = new Discord.MessageActionRow()
+    let row = new Discord.ActionRowBuilder()
         .addComponents(
-            new Discord.MessageButton()
+            new Discord.ButtonBuilder()
                 .setLabel("Vote for me")
                 .setURL("https://top.gg/bot/798144456528363550/vote")
-                .setStyle("LINK"),
+                .setStyle(Discord.ButtonStyle.Link),
         );
 
     dbl.hasVoted(interaction.user.id).then(voted => {
         if (voted) {
-            client.sendEmbed({
-                title: '<:ticketzz:1071793049396584589>・Vote',
-                desc: 'You have voted!',
-                image: 'https://cdn.discordapp.com/attachments/1064478382504550400/1072035974781091960/CLUDO-removebg-preview.png',
+            client.embed({
+                title: `<:ticketzz:1071793049396584589>・Vote`,
+                desc: `You have voted!`,
+                image: `https://cdn.discordapp.com/attachments/1064478382504550400/1072035974781091960/CLUDO-removebg-preview.png`,
                 color: client.config.colors.succes,
                 components: [row],
                 type: 'editreply'
-            }, interaction);
-        } else {
-            client.sendEmbed({
-                title: '<:ticketzz:1071793049396584589>・Vote',
-                desc: 'You have not voted!',
-                image: 'https://cdn.discordapp.com/attachments/1064478382504550400/1072035974781091960/CLUDO-removebg-preview.png',
+            }, interaction)
+        }
+        if (!voted) {
+            client.embed({
+                title: `<:ticketzz:1071793049396584589>・Vote`,
+                desc: `You have not voted!`,
+                image: `https://cdn.discordapp.com/attachments/1064478382504550400/1072035974781091960/CLUDO-removebg-preview.png`,
                 color: client.config.colors.error,
                 components: [row],
                 type: 'editreply'
-            }, interaction);
+            }, interaction)
         }
-    }).catch(error => {
-        client.errNormal({ text: 'There was an error by checking this vote!', editreply: true }, interaction);
-    });
-}
-
+    }).catch(error => { client.errNormal({ text: `There was an error by checking this vote!`, editreply: true }, interaction) });
  
 
  
