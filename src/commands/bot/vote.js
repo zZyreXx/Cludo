e277742db4ea6afb0cbf068554f13fc612cfd22d
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-02-10 20:08:57 +0530

    Update vote.js
    Fix

--- src/commands/bot/vote.js
+++ src/commands/bot/vote.js
@@ -1,35 +1,39 @@
-const { MessageActionRow, MessageButton } = require('discord.js');
-const Topgg = require('@top-gg/sdk');
-const moment = require('moment');
-require('moment-duration-format');
+const Discord = require('discord.js');
+const Topgg = require(`@top-gg/sdk`);
+const moment = require("moment");
+require("moment-duration-format");
 
 module.exports = async (client, interaction, args) => {
-    let dbl = new Topgg.Api(process.env.TOPGG_TOKEN);
+    let dbl = new Topgg.Api(process.env.TOPGG_TOKEN)
 
-    let row = new MessageActionRow()
+    let row = new Discord.ActionRowBuilder()
         .addComponents(
-            new MessageButton()
+            new Discord.ButtonBuilder()
                 .setLabel("Vote for me")
                 .setURL("https://top.gg/bot/798144456528363550/vote")
-                .setStyle('LINK')
+                .setStyle(Discord.ButtonStyle.Link),
         );
 
-    try {
-        const voted = await dbl.hasVoted(interaction.user.id);
-
-        let embed = {
-            title: '📨・Vote',
-            image: { url: 'https://cdn.discordapp.com/attachments/843487478881976381/874694192755007509/Bot_banner_vote.jpg' },
-            color: voted ? client.config.colors.success : client.config.colors.error,
-            description: voted ? 'You have voted!' : 'You have not voted!',
-            timestamp: new Date()
-        };
-
-        await interaction.reply({ embeds: [embed], components: [row] });
-    } catch (error) {
-        console.error('Error occurred while checking vote:', error);
-        await interaction.reply({ content: 'There was an error checking your vote!', ephemeral: true });
-    }
-};
-
-
+    dbl.hasVoted(interaction.user.id).then(voted => {
+        if (voted) {
+            client.embed({
+                title: `📨・Vote`,
+                desc: `You have voted!`,
+                image: `https://cdn.discordapp.com/attachments/843487478881976381/874694192755007509/Bot_banner_vote.jpg`,
+                color: client.config.colors.succes,
+                components: [row],
+                type: 'editreply'
+            }, interaction)
+        }
+        if (!voted) {
+            client.embed({
+                title: `📨・Vote`,
+                desc: `You have not voted!`,
+                image: `https://cdn.discordapp.com/attachments/843487478881976381/874694192755007509/Bot_banner_vote.jpg`,
+                color: client.config.colors.error,
+                components: [row],
+                type: 'editreply'
+            }, interaction)
+        }
+    }).catch(error => { client.errNormal({ text: `There was an error by checking this vote!`, editreply: true }, interaction) });
+}
