73fab9e038c7f2da62c49cc578e6994f4672ac1c
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-02-14 09:08:10 +0530

    Update changelogs.js
    Fix Emoji

--- src/commands/bot/changelogs.js
+++ src/commands/bot/changelogs.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 
 module.exports = async (client, interaction, args) => {
     client.embed({
-        title: "<:logzz:1071794113982255134>・Changelogs",
+        title: "<:settings:1205543029817999390><:arrowright:1205414889451954196> Changelogs",
         desc: `_____`,
         thumbnail: client.user.avatarURL({ size: 1024 }),
         fields: [{
-            name: "<:logzz:1071794113982255134>┆Changelogs",
+            name: "<:multi:1205408937831309344><:arrowright:1205414889451954196>Changelogs",
                 value: '24/01/2024 - Added [Mallu Radio Station Club FM 99.6](https://clubfm.ae/)\n -Fixed Minor Bugs\n -Changed Host\n -Updated bot Latest Disocrd.js/Music Version 0.16.1',
                 inline: false,
             },
