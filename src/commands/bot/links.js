480132eea50341ebf059efad544774e696df1566
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-02-14 09:28:00 +0530

    Update links.js
    fix Emoji and Bg

--- src/commands/bot/links.js
+++ src/commands/bot/links.js
@@ -5,39 +5,39 @@ module.exports = async (client, interaction, args) => {
         .addComponents(
             new Discord.StringSelectMenuBuilder()
                 .setCustomId('Bot-linkspanel')
-                .setPlaceholder('<:helpzz:1072010469142974504>┆Nothing selected')
+                .setPlaceholder('Navigation System')
                 .addOptions([
                     {
                         label: `Support server`,
                         description: `Join the suppport server`,
-                        emoji: "<:helpzz:1072010469142974504>",
+                        emoji: "<:__:1205407307811262466>",
                         value: "support-linkspanel",
                     },
                     {
                         label: `Invite Bot`,
                         description: `Invite Bot to your server`,
-                        emoji: "<:linkee:1071784295594737768>",
+                        emoji: "<:linkee:1205416083469762610>",
                         value: "invite-linkspanel",
                     },
                     {
                         label: `Community Server`,
                         description: `Join the community server!`,
-                        emoji: "<:serverzz:1071796723128746084>",
+                        emoji: "<:globe:1205409366849888307>",
                         value: "community-linkspanel",
                     },
                     {
                         label: `Top.gg`,
                         description: `Show the top.gg link`,
-                        emoji: "<:botzz:1071794843438825492>",
+                        emoji: "<:automod:1205540532206575666>",
                         value: "top.gg-linkspanel",
                     },
                 ]),
         );
 
     client.embed({
-        title: `<:linkee:1071784295594737768>・Links`,
+        title: `<:linkee:1205416083469762610><:arrowright:1205414889451954196>Links`,
         desc: `Get access to all Bot links! Choose the link you need in the menu below`,
-        image: "https://cdn.discordapp.com/attachments/1064478382504550400/1072035974781091960/CLUDO-removebg-preview.png",
+        image: "https://cdn.discordapp.com/attachments/1064478382504550400/1205446107279265812/CLUDO_1_2.png?ex=65d8662c&is=65c5f12c&hm=1dc3f94e83b96c83f3399ac2dafa88fa8c7a9479276a80371507b09e93e79950&",
         components: [row],
         type: 'editreply'
     }, interaction)
