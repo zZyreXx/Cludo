d3268ecb7a6bd0dbbd740548053662b39f13689e
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-02-18 16:59:14 +0530

    Update changelogs.js
    Fix

--- src/commands/bot/changelogs.js
+++ src/commands/bot/changelogs.js
@@ -7,7 +7,7 @@ module.exports = async (client, interaction, args) => {
         thumbnail: client.user.avatarURL({ size: 1024 }),
         fields: [{
             name: "<:multi:1205408937831309344><:arrowright:1205414889451954196>Changelogs",
-                value: '24/01/2024 - Added [Mallu Radio Station Club FM 99.6](https://clubfm.ae/)\n -Fixed Minor Bugs\n -Changed Host\n -Updated bot Latest Disocrd.js/Music Version 0.16.1',
+                value: '24/01/2024 - No info From the Dev',
                 inline: false,
             },
         ],
