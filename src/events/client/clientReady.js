76885e82658b78eb4d66644c43792925610a6270
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-02-23 20:22:01 +0530

    Update clientReady.js
    fix

--- src/events/client/clientReady.js
+++ src/events/client/clientReady.js
@@ -35,7 +35,7 @@ module.exports = async (client) => {
       ];
     }
     const randomText = statuttext[Math.floor(Math.random() * statuttext.length)];
-    client.user.setPresence({ activities: [{ name: randomText, type: Discord.ActivityType.Watching }], status: 'Idle' });
+    client.user.setPresence({ activities: [{ name: randomText, type: Discord.ActivityType.Watching }], status: 'dnd' });
   }, 50000)
 
   client.player.init(client.user.id);
