a04b27d0abf6ca7a6b67a617f6262c1b87cbfcf8
Tɑιғʏὗ™ <93309693+zZyreXx@users.noreply.github.com>
2024-04-30 09:55:22 +0530

    Update create.js
    added @everyone

--- src/commands/announcement/create.js
+++ src/commands/announcement/create.js
@@ -4,10 +4,17 @@ module.exports = async (client, interaction, args) => {
     const message = interaction.options.getString('message');
     const channel = interaction.options.getChannel('channel');
 
+    // Mention @everyone outside the embed
+    const everyoneMention = '@everyone';
+
+    // Send the announcement with the embed
     client.embed({ 
         title: `<:megaz:1205540030055981116><:arrowright:1205414889451954196>Announcement!`, 
         desc: message 
     }, channel);
+
+    // Send the announcement without the embed, mentioning @everyone
+    await channel.send(`${everyoneMention}\n${message}`);
 
     client.succNormal({
         text: `Announcement has been sent successfully!`,
@@ -20,4 +27,3 @@ module.exports = async (client, interaction, args) => {
         type: 'editreply'
     }, interaction);
 }
- 
